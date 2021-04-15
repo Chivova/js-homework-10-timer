611467ae15c0c6923d04c70cabb4e84f768e7212
Chivova <Chivovaz@ua.fm>
2021-04-15 22:40:13 +0300

    update index.js, index.html, package-lock.json, hw must complite

--- src/index.js
+++ src/index.js
@@ -6,28 +6,44 @@ const refs = {
 };
 
 class CountdownTimer {
-  constructor() {
+  constructor({ targetDate, onTick }) {
     this.timerId = null;
+    this.targetDate = targetDate;
+    this.onTick = onTick;
   }
 
   timerStart() {
+    const finaleDate = this.targetDate;
+
     this.timerId = setInterval(() => {
       const currentDate = new Date();
-      const target = targetDate - currentDate;
-      console.log(target);
+      const amountTime = finaleDate - currentDate;
+      const time = this.getTimerComponents(amountTime);
+
+      this.onTick(time);
     }, 1000);
   }
-  getTimerComponents() {
+  getTimerComponents(time) {
     const days = Math.floor(time / (1000 * 60 * 60 * 24));
     const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     const secs = Math.floor((time % (1000 * 60)) / 1000);
+
+    return { days, hours, mins, secs };
   }
 }
 
 const timer = new CountdownTimer({
+  onTick: dateOutputFace,
   selector: '#timer-1',
-  targetDate: new Date('2022 december 31 20:00'),
+  targetDate: new Date('2021 december 31 20:00'),
 });
 
 timer.timerStart();
+
+function dateOutputFace({ days, hours, mins, secs }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${mins}`;
+  refs.seconds.textContent = `${secs}`;
+}
